3c5d1e911600d6fcd47edab337dc453ae5936ba7
Mook <mookjpy@gmail.com>
2015-03-06 14:47:03 +0100

    Add namespace

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,3 +4,4 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 #resque_config = YAML.load_file(rails_root + '/config/resque.yml')
 resque_config = YAML.load(ERB.new(IO.read(rails_root + '/config/resque.yml')).result)
 Resque.redis = resque_config[rails_env]
+Resque.redis.namespace = "resque:#{Rails.env}"
