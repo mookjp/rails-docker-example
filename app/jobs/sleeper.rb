99c057f2800e0d3f3e7c2a34dfb8d43cd48fe128
Mook <mookjpy@gmail.com>
2015-03-06 14:46:39 +0100

    Change to write log to file

--- app/jobs/sleeper.rb
+++ app/jobs/sleeper.rb
@@ -1,8 +1,12 @@
 class Sleeper
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
   @queue = :sleep
+  @logger = Logger.new(File.join(Rails.root, 'log', 'resque.log'))
 
   def self.perform(seconds)
     # TODO: Add message to logger.
+    @logger.debug 'Start sleeping...'
     sleep(seconds)
+    @logger.debug 'Finished sleeping'
   end
 end
